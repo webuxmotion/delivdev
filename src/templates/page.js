d4b6088f2de832c1b2fc80892e4bb86be1558183
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2021-08-01 14:02:27 +0300

    Initial commit

---
+++ src/templates/page.js
@@ -0,0 +1,38 @@
+import { graphql } from 'gatsby';
+import { RichText } from 'prismic-reactjs';
+import React from 'react';
+import Layout from '../components/layout';
+
+export const query = graphql`
+  query PageQuery($id: String) {
+    prismic {
+      allPages(id: $id) {
+        edges {
+          node {
+            page_title
+            content
+            _meta {
+              uid
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Page = (props) => {
+  const node = props.data.prismic.allPages.edges[0].node;
+  const pageTitle = node.page_title;
+  const content = node.content;
+  
+  return (
+    <Layout>
+      <RichText render={pageTitle} />
+      <RichText render={content} />
+    </Layout>
+  )
+}
+
+export default Page;
